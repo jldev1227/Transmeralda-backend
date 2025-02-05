ce4eb3e83c46548da2f6ba418e1a8ed32eb5cbcc
jldev1227 <1227jldev@gmail.com>
2025-02-05 10:32:30 -0500

    hotfix - modificación de los datos del jwt

--- src/helpers/generarJWT.js
+++ src/helpers/generarJWT.js
@@ -1,9 +1,18 @@
 import jwt from 'jsonwebtoken'
 
-const generatJWT = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET, {
-        expiresIn: '30d',
-    })
-}
+const generatJWT = (usuario) => {
+  return jwt.sign(
+    {
+      id: usuario.id,
+      nombre: usuario.nombre,
+      apellido: usuario.apellido,
+      correo: usuario.correo,
+      rol: usuario.rol,
+      // Puedes incluir correo u otros datos que necesites
+    },
+    process.env.JWT_SECRET, // Asegúrate de tener tu secreto en variables de entorno
+    { expiresIn: '30d' }     // Ajusta el tiempo de expiración que necesites
+  );
+};
 
 export default generatJWT;
